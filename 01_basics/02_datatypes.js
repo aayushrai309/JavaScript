c467a12c959433d05dd42146671e5d6b33e190ea
aayushrai309 <155513594+aayushrai309@users.noreply.github.com>
2024-10-07 10:33:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "Aayush Rai"; //string
+let age = 25; // integer
+let isLoggedin = false; //boolean
+let city; //undefined
+let state = null; //null
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedin);
+console.log(typeof city);
+console.log(typeof state);
