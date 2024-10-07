4add7b4d5be1104fa9a6053936128dec032b0282
aayushrai309 <155513594+aayushrai309@users.noreply.github.com>
2024-10-07 10:32:12 +0000

    variables datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 24515;
+let accountEmail = "aayushrai@gmail.com";
+var accountPass = "aayush112";
+accountCity = "Boisar";
+
+accountEmail = "aayush@google.com";
+accountPass = 124563;
+accountCity = "Palghar";
+
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
