763050fa96dec63af73f741ab975f4750eb492fa
aayushrai309 <155513594+aayushrai309@users.noreply.github.com>
2024-10-07 11:36:43 +0000

    conversionOperatiions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score = "33";
+console.log(typeof score);
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+let isLoggedin = 1;
+let booleanisLoggedin = Boolean(isLoggedin);
+console.log(booleanisLoggedin);
+
+
+
+
+// '33' => 33
+// "33abc" => NaN
+// true => 1; false => 0;
