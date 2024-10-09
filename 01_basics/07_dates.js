ae36bd8efcfa6d7236f5beca7d59ed5319bc33ad
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-09 11:18:37 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -7,3 +7,9 @@ console.log(myDate.toISOString());
 console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 
+let myCreateDate = new Date(2024, 8, 30); // in javascript months starts with zero
+console.log(myCreateDate);
+console.log(myCreateDate.toString());
+
+let myCreateTime = new Date(2024, 8, 30, 5, 30);
+console.log(myCreateTime.toString()); // date is given in the form of year month day hour minutes and seconds
