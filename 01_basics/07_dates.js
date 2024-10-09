bde8b830600ce3c63373a588457d465204636f19
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-09 11:10:12 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,1 +1,9 @@
 // -------------------------------Dates-------------------------------------//
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+
