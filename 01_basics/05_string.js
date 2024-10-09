a04d5fc9a7546b90f39892ecfef06da82cf7fa8f
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-09 09:41:55 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name = "Aayush Rai";
+const repoCount = 50;
+
+console.log(name + " is " + repoCount);
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+// these are string methods/builders
+console.log(name.length); //to find length of string
+console.log(name.toUpperCase()); // to convert string to Upper Case
+console.log(name.toLowerCase()); // to convert string to Lower Case
+console.log(name.charAt(3)); // to find the which letter is at that position 
+console.log(name.indexOf("s")); // to find the index of the passed value
