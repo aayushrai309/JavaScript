b8b3e1b5cfb3d73ac3bac549abd0557aa5dc4672
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-11 11:45:18 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,15 +1,38 @@
 // singleton object = jab bhi koi object constructor ki help se banega tab wo singleton object banega (Object.create)
 
 // object literals
+
+const mySym = Symbol("key1"); // first you have to define a symbol out side the object then you can access the symbol 
+
 const user = {
     name : "Aayush",
     "fullname": "Aayush Rai",
+    [mySym]: "mykey1", // correct syntax to define an symbol in object
     age: 24,
     email: "aayush@google.com",
     pass: "Aayu123",
     isLogged: false,
 };
 
+console.log("org object = ", user);
+console.log(user.isLogged); 
+console.log(user["fullname"]); // special way to access some keys fro  objects.
+console.log(user[mySym]);
+
+user.fullname = "Aayush Sanjay Rai"; // correct way to over write the value of objects
+user.email = "aayush@gmail.com";
 console.log(user);
-console.log(user.isLogged); //not a good way to acces objects as for "fullname"
-console.log(user["fullname"]);
+
+// Object.freeze(user); // freez method is used to freez the object for doing the changes
+
+
+// function to access key from objects
+user.greeting = function () {
+    console.log("Hello Everyone");
+}
+console.log(user.greeting());
+
+user.greetingTwo = function () {
+    console.log(`Hello ${this.name}`); // this keyword is used to access properties from element
+}
+console.log(user.greetingTwo());
