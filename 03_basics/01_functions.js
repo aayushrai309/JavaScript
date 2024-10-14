926a1fa9bcc144aacac4af5754bfdfc96de8e9c8
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-14 07:53:49 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,4 @@
+//function =  a function is a block of code that performs a specific task or returns a value. 
+//            Functions are used to group code snippets together and reuse them throughout a program. 
+//            They can also be used to produce different results by using the same code with different arguments
+
