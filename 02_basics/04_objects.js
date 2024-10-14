c7fad6937a439adb315cb4ca87b2df1de6ed773a
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-14 06:54:47 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,41 @@
+// how to declare objects using constructor (singleton objects)
+
+// const tinderUser = new Object(); // this is a singleton object.
+
+const tinderUser = {}; // this is a non singleton object.
+tinderUser.id = "123abc";
+tinderUser.name = "Aayush Rai";
+tinderUser.isLogged = false;
+
+console.log(tinderUser);
+
+
+const regularUser = { 
+    email: "aayush@google.com",
+    fullName : { 
+        userfullname: { // we can define object inside an object.
+            firstName: "Aayush",
+            lastName: "Rai",
+        }
+    }
+}
+
+console.log(regularUser);
+console.log(regularUser.fullName); // we can access the value of object inside an Object by using dot(.) and element name.
+console.log(regularUser.fullName.userfullname.firstName);
+console.log(regularUser.fullName.userfullname.lastName); // we can access the value of object inside an Object by using dot(.) and element name.
+console.log(regularUser);
+
+
+//------------------------------mearging two objects-----------------------------------//
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+
+// const obj3 = {obj1, obj2}; // syntax will show an problem it will mearge both the array along with the variable name.
+
+
+console.log(obj3);
+
+
+
+
