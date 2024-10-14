1fdf628bc1ee657da615fbfe3b17dcf0f73f4791
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-14 11:45:20 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,3 +13,31 @@ sayMyName(); // this is the execuation of the function
 sayMyName(); // in short function means defining once and executing multiple times
 
 
+
+
+//---------------------------Function to add two numbers----------------------------------//
+
+function addTwoNumbers(num1, num2) {
+    let result = num1 + num2;
+    return result; // Note - Return ke baad koi bhi value print nhi hogi. because it has returned the value of result.
+}
+
+const result = addTwoNumbers(3, 4);
+console.log(result);
+
+
+
+//-------------------------Function to diaplay Message-----------------------------------//
+
+function loginUserMessage(username = "Sam") { // Note = Jab bhi koi value pass nahi ki jayegi tab function automatically value as "Sam" le lega.
+    if(!username){
+        console.log("Please enter a user name");
+        return
+    }
+    return `${username} just logged in.`
+}
+
+console.log(loginUserMessage("Aayush Rai"));
+console.log(loginUserMessage()); // Note = Jab bhi aap fuunction me koi value nhi define karte hai tab output me undefined aata hai
+
+
