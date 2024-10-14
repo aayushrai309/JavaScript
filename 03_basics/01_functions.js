1b52ccfb6911ab39371218fc837d8b57977a9d16
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-14 07:58:34 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,3 +2,11 @@
 //            Functions are used to group code snippets together and reuse them throughout a program. 
 //            They can also be used to produce different results by using the same code with different arguments
 
+
+function sayMyName() {
+    console.log("Aayush Rai");
+}
+
+// sayMyName => this is the reference of the function
+
+sayMyName() // this is the execuation of the function
