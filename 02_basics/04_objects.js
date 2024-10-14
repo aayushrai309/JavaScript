9a785f823d45762d164be8eca2a5fa0318e2f00a
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-14 07:01:51 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -32,8 +32,9 @@ const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
 
 // const obj3 = {obj1, obj2}; // syntax will show an problem it will mearge both the array along with the variable name.
+// const obj3 = Object.assign(obj1, obj2); // it will mearge both the Objects.
 
-
+const obj3 = Object.assign({}, obj1, obj2); // {} = parenthesis works as a optional parameter and it will definately mearge bot the objects and gives us the desired result. 
 console.log(obj3);
 
 
