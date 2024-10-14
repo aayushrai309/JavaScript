c985d5f491ee18884280002132948a1b503a654e
Aayush Rai <155513594+aayushrai309@users.noreply.github.com>
2024-10-14 07:03:23 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -34,6 +34,7 @@ const obj2 = {3: "c", 4: "d"};
 // const obj3 = {obj1, obj2}; // syntax will show an problem it will mearge both the array along with the variable name.
 // const obj3 = Object.assign(obj1, obj2); // it will mearge both the Objects.
 
+//The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
 const obj3 = Object.assign({}, obj1, obj2); // {} = parenthesis works as a optional parameter and it will definately mearge bot the objects and gives us the desired result. 
 console.log(obj3);
 
